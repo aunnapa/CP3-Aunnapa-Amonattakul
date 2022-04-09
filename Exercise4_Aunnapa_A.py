713efe624514fb85be4e03244afa1a475c347e4e
aunnapa <aunnapa@sosscorp.com>
2022-04-09 12:25:21 +0700

    Exercise4

---
+++ Exercise4_Aunnapa_A.py
@@ -0,0 +1,10 @@
+score_foundation_english = 3.0
+score_general_business = 2.0
+score_computer_systems = 2.0
+score_computer_programming = 4.0
+
+print('---Your Score---')
+print('Foundation English : คะแนนที่ได้ ',score_foundation_english)
+print('General Business : คะแนนที่ได้ ',score_general_business)
+print('Introduction to Computer Systems : คะแนนที่ได้ ',score_computer_systems)
+print('Computer Programming : คะแนนที่ได้ ',score_computer_programming)

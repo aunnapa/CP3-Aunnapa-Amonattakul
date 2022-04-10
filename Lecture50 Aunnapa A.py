eb9732177045ce3943eefe5e4abd61648e9bffba
aunnapa <aunnapa@sosscorp.com>
2022-04-10 18:36:07 +0700

    Lecture50

---
+++ Lecture50 Aunnapa A.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print('x + y =',x+y)
+def minusNumber(x,y):
+    print('x - y =',x-y)
+def mulNumber(x,y):
+    print('x * y =',x*y)
+def divNumber(x,y):
+    print('x / y =',x/y)
+addNumber(100,10)
+minusNumber(100,10)
+mulNumber(100,10)
+divNumber(100,10)
